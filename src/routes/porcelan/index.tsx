825a31f6d95358125148b2bd332dea26dd63da9a
ania orwatk <orwat.ania@gmail.com>
2023-02-02 10:55:18 +0100

    finish shop

--- src/routes/porcelan/index.tsx
+++ src/routes/porcelan/index.tsx
@@ -1,5 +1,6 @@
-import { component$, useClientEffect$, useStore } from '@builder.io/qwik';
-import { DocumentHead, useLocation } from '@builder.io/qwik-city';
+import { component$, useClientEffect$, useContext, useStore } from '@builder.io/qwik';
+import { DocumentHead, useContent, useLocation } from '@builder.io/qwik-city';
+import { MyContext } from '~/root';
 
 
 export default component$(() => {
@@ -12,12 +13,21 @@ export default component$(() => {
         url: ""
     });
 
+    const contextState = useContext(MyContext);
+    console.log(contextState)
+
     useClientEffect$(() => {
         const data = JSON.parse(localStorage.getItem("porc"))
         state.name = data.name
         state.url =data.url
         state.price= data.price
     });
+
+    useClientEffect$(()=>{
+        if (localStorage.getItem('porcelanin-basket')){
+            contextState.items =[...JSON.parse(localStorage.getItem('porcelanin-basket')).items]
+        }
+    })
 
     return (
         <div class="flex flex-col gap-4">
@@ -31,12 +41,15 @@ export default component$(() => {
                 if(localStorage.getItem('porcelanin-basket')){
                     currBasket =JSON.parse(localStorage.getItem('porcelanin-basket'))
                 }
-                currBasket.items.push([state])
+                currBasket.items.push(state)
                 localStorage.setItem('porcelanin-basket', JSON.stringify(currBasket))
+                contextState.items =[...contextState.items, state]
             }
 
             } class="border border-slate-900 border-solid px-8 mx-auto py-2 hover:opacity-50">BUY ITEM</button>
-          
+          {/* {contextState.items.map((element)=>{
+            return <div>{element.name}</div>
+          })} */}
         </div>
     );
 });
